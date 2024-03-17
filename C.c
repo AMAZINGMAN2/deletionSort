7d570ddb6ead5a5891b39ce2afb7e58599cf6589
Hashim Almuqbel <85612244+AMAZINGMAN2@users.noreply.github.com>
2024-03-17 09:16:05 +0300

    Create C.c

---
+++ C.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void deleteArray(int** arr, int* size) {
+    free(*arr);
+    *arr = NULL;
+    *size = 0;
+}
+
+int main() {
+    int arr[] = {12, 45, 23, 9, 37, 18, 61, 2, 25, 31, 7, 16, 48, 29, 41, 5, 22, 35, 14, 27, 39, 3, 56, 11, 33, 20, 54, 8, 43, 28, 51, 6, 17, 38, 10, 34, 44, 26, 50, 15};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    printArray(arr, size);
+
+    deleteArray(&arr, &size);
+
+    printf("After 'deletionsort':\n");
+    if (arr == NULL) {
+        printf("Array is deleted.\n");
+    } else {
+        printArray(arr, size);
+    }
+
+    return 0;
+}
